d15196605d853236ccdb4e87db1f5632f2821ea6
Makenchii <marwane422@hotmail.fr>
2025-02-11 14:54:59 +0100

    first

---
+++ client/src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-blue-600 text-white p-4 text-center">
+      <p>© 2024 Mon Application</p>
+    </footer>
+  );
+}
---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <header className="app-header">
+      <div className="header-content">
+        <h1>Mon Application</h1>
+      </div>
+    </header>
+  );
+}
